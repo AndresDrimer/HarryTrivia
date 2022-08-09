317347bee5447dd7415dd8aa513befe2ebbedb3e
AndresDrimer <andresdrimer@hotmail.com>
2022-08-09 12:19:57 -0300

    api conectada via VUE, aun no conecta con select

--- scriptCarta.js
+++ scriptCarta.js
@@ -1,29 +1,58 @@
 // CARTA promise
 
-const urlTodos=
-"http://hp-api.herokuapp.com/api/characters"
 
-async function traerCarta(){
-    const response = await fetch(urlTodos)
-    const data = await response.json()
-    const {name, actor, image, house,yearOfBirth} = data
-    
-//    aca no hay azar, siemrpe es la de harry
-    let nombre=data[0].name
-    let actore=data[0].actor
-    let casa = data[0].house
-    let anio = data[0].yearOfBirth
-    let imagen = data[0].image
+
+
     // console.log(imagen)  
     // document.getElementById("fotoCarta").style.backgroundImage = "url(imagen)"
     
-    var names = document.querySelector('#selectName');
-    names.innerHTML = '' + 
-      '<option value="nombre">'nombre'</option>'
+    // var names = document.querySelector('#selectName');
+    // names.innerHTML = '' + 
+    //   '<option value="nombre">'nombre'</option>'
       
-}
+//     function random(min, max) {
+//         return Math.floor((Math.random() * (max - min + 1)) + min);
+//     }
 
-traerCarta()
+
+let datosCarta = new Vue({
+    el: "#selectName",
+    data: {
+        urlTodos: "http://hp-api.herokuapp.com/api/characters" ,
+        nombre: "",
+        actore: "",
+        casa: "",
+        anio: "",
+        imagen: ""
+
+    },
+    methods: {
+        async traerCarta() {
+        const response = await fetch(this.urlTodos)
+        const data = await response.json()
+        const {name, actor, image, house,yearOfBirth} = data
+        console.log(data[0].name)
+//    aca no hay azar, siemrpe es la de harry
+        this.nombre=data[0].name
+        this.casa = data[0].house
+        this.anio = data[0].yearOfBirth
+        this.imagen = data[0].image
+        this.actore=data[0].actor
+        console.log(this.anio)
+        },
+    },
+})
+
+
+    // let imgCarta = document.getElementById("fotoCarta")
+    // let fotitoCarta = document.createElement("fotitoCartita")
+    // fotitoCarta.setAttribute("src", imagen)
+    // imgCarta.appendChild(fotitoCarta) 
+
+
+datosCarta.traerCarta()
+
+
    
     
     
